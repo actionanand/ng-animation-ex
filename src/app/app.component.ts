3b19b2f47c0f7f9ff2bce8936e33a359a8dee32d
AnandRaja-tech <anand.official.in@gmail.com>
2022-01-19 11:05:56 +0000

    animation with in-between state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,31 @@ import { trigger , state , style, transition, animate } from '@angular/animation
       })),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(700))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),  
+      state('highlighted', style({
+        backgroundColor: 'green',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal <=> highlighted', animate(600)),
+      transition('shrunken <=> *', animate(500, style({
+        borderRadius: '50px'
+      })))
     ])
   ]
 })
 export class AppComponent {
   list = ['Milk', 'Sugar', 'Bread'];
   state = 'normal';
+  wildState = 'normal';
 
     onAdd(item: string) {
       this.list.push(item);
@@ -29,5 +48,10 @@ export class AppComponent {
 
     onAnimate() {
       this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+      this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+    }
+
+    onShrink() {
+      this.wildState = 'shrunken';
     }
 }
