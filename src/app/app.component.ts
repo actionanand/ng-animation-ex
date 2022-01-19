4e69c7b162eea9be0a0fdb094c055688a28f6edf
AnandRaja-tech <anand.official.in@gmail.com>
2022-01-19 10:20:38 +0000

    basic angular animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { trigger , state , style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),  
+      state('highlighted', style({
+        backgroundColor: 'green',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(700))
+    ])
+  ]
 })
 export class AppComponent {
   list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal';
 
-    onAdd(item) {
+    onAdd(item: string) {
       this.list.push(item);
+    }
+
+    onAnimate() {
+      this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
     }
 }
